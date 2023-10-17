b754c4707095a4d3af749f0cf95452fe3ab73064
Fernando Etchepare <fernando.etchepare@globallogic.com>
2023-10-17 11:46:32 -0300

    Allow H2 console

--- src/main/java/com/globallogic/bci/exercise/security/config/SecurityConfig.java
+++ src/main/java/com/globallogic/bci/exercise/security/config/SecurityConfig.java
@@ -39,7 +39,7 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-        http.csrf(csrf -> csrf.disable())
+        http.csrf(csrf -> csrf.disable()).headers().frameOptions().disable().and()
             .authorizeRequests(requests -> requests.antMatchers("/h2-console/**", "/api/v1/sign-up/**", "/login/**").permitAll()
             .anyRequest().authenticated()).sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .addFilterBefore(jwtAuthorizationFilter, UsernamePasswordAuthenticationFilter.class);
