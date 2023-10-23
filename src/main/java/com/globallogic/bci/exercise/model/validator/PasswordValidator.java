29dbfb370ab959cbcae1e335a3e5b489e8afa1a8
Fernando Etchepare <fernando.etchepare@globallogic.com>
2023-10-23 10:24:27 -0300

    Class properties to variables

--- src/main/java/com/globallogic/bci/exercise/model/validator/PasswordValidator.java
+++ src/main/java/com/globallogic/bci/exercise/model/validator/PasswordValidator.java
@@ -8,24 +8,17 @@ import javax.validation.ConstraintValidatorContext;
 
 public class PasswordValidator implements ConstraintValidator<ValidPassword, String> {
 
-	private Pattern pattern;
-	private Matcher matcher;
-
 	// Rules: Lowercase letters + Just 1 uppercase letter + 2 numbers, between 8 and 12 characters.
 	private static final String PASSWORD_PATTERN = "^(?=[^A-Z]*[A-Z][^A-Z]*$)(?=[^0-9]*[0-9]{2}[^0-9]*$)[a-zA-Z0-9]{8,12}$";
 
-	@Override
-	public void initialize(ValidPassword constraintAnnotation) {
-	}
-
-	@Override
+    @Override
 	public boolean isValid(String pass, ConstraintValidatorContext context) {
 		return (validatePassword(pass));
 	}
 
 	private boolean validatePassword(String pass) {
-		pattern = Pattern.compile(PASSWORD_PATTERN);
-		matcher = pattern.matcher(pass);
+		Pattern pattern = Pattern.compile(PASSWORD_PATTERN);
+		Matcher matcher = pattern.matcher(pass);
 		return matcher.matches();
 	}
 
