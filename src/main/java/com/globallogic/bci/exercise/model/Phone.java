66ec539dea4f7dada86486cc47a70e6145398c34
Fernando Etchepare <fernando.etchepare@globallogic.com>
2023-10-26 20:24:11 -0300

    import complete persistence library

--- src/main/java/com/globallogic/bci/exercise/model/Phone.java
+++ src/main/java/com/globallogic/bci/exercise/model/Phone.java
@@ -1,10 +1,6 @@
 package com.globallogic.bci.exercise.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 import lombok.*;
 
