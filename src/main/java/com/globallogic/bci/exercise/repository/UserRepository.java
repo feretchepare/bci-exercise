6756d454ee41fd3597c322cf209b3684d5ac82d8
Fernando Etchepare <fernando.etchepare@globallogic.com>
2023-10-26 20:24:39 -0300

    Custom query to get phones

--- src/main/java/com/globallogic/bci/exercise/repository/UserRepository.java
+++ src/main/java/com/globallogic/bci/exercise/repository/UserRepository.java
@@ -3,11 +3,13 @@ package com.globallogic.bci.exercise.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.globallogic.bci.exercise.model.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
+	@Query("SELECT u FROM User u LEFT JOIN FETCH u.phones WHERE u.email = ?1")
 	Optional<User> findOneByEmail(String email);
 }
